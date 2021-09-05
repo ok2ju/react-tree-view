eba07553c82926488d495419a6b04b24264981df
ok2ju <soulSpringg@gmail.com>
2021-09-05 15:42:30 +0300

    feature: Make traverseTree as carried fn

--- src/TreeView/utils.js
+++ src/TreeView/utils.js
@@ -1,6 +1,18 @@
 export const reducer = (state, newState) => ({ ...state, ...newState });
 
-export const traverseTree = (reducerFn) => (initialValue, currItem) => {
+const curry = (fn) => {
+  return function curried(...args) {
+    if (args.length >= fn.length) {
+      return fn(...args);
+    }
+
+    return (...args1) => {
+      return curried(...(args.concat(args1)));
+    };
+  };
+};
+
+export const traverseTree = curry((reducerFn, initialValue, currItem) => {
   if (Array.isArray(currItem)) {
     return currItem.reduce(traverseTree(reducerFn), [])
   }
@@ -12,4 +24,4 @@ export const traverseTree = (reducerFn) => (initialValue, currItem) => {
   }
 
   return currItem.children.reduce(traverseTree(reducerFn), acc);
-};
+});
